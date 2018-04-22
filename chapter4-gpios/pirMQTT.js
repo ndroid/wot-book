394f9232492cbe8212da791fe662613b76f8ac25
ndroid <chris.miller.nd@gmail.com>
2018-04-22 12:35:39 -0400

    updated MQTT pir file to publish

--- chapter4-gpios/pirMQTT.js
+++ chapter4-gpios/pirMQTT.js
@@ -1,9 +1,28 @@
+var mqttKey = '7VH7PX53O3YXB9AN';
+var channelKey = 'QNZX1HHTT68JGLPF';
+var channelID = '480910';
+var fieldNumber = 'field3';
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtts://mqtt.thingspeak.com:1883', {username: 'miller4', password: mqttKey});
+var topic = 'channels/'+channelID+'/publish/fields/'+fieldNumber+'/'+channelKey;
+var lastValue=2;
 var Gpio = require('onoff').Gpio,
-  sensor = new Gpio(17, 'in', 'both');    //#A
+	sensor = new Gpio(17, 'in', 'both', {debounceTimeout: 10});    //#A
+ 
+client.on('connect', function () {
+  console.log('MQTT connection established');
+//  client.subscribe('presence');
+//  client.publish('presence', 'Hello mqtt')
+});
 
 sensor.watch(function (err, value) { //#B
   if (err) exit(err);
-  console.log(value ? 'there is someone!' : 'not anymore!');
+  if (lastValue != value) {
+    console.log(value ? 'present' : 'absent');
+    if (client.connected())
+      client.publish(topic, value);
+  }
+  lastValue = value;
 });
 
 function exit(err) {
